ac42d4880b357c0527fd69d102aa814541bee63d
Taha Sari <saritaha1907@gmail.comgit config --global user.email saritaha1907@gmail.comgit config --global user.email saritaha1907@gmail.com>
2022-10-30 17:09:08 +0300

    Commit-1 translated to english

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
      Scanner scanner =new Scanner(System.in);
-     double kilo,boy,endeks;
-     System.out.println("Lütfen kilonuzu giriniz:");
-     kilo= scanner.nextDouble();
-     System.out.println("Lütfen boyunuzu(metre cinsinden)giriniz:");
-     boy= scanner.nextDouble();
-     endeks=kilo/(boy*boy);
-     System.out.println("Vücut kitle endeksiniz:"+endeks);
+     double weight,height,ındex;
+     System.out.println("Please enter your weight:");
+     weight= scanner.nextDouble();
+     System.out.println("Please enter your height in meters:");
+     height= scanner.nextDouble();
+     ındex=weight/(height*height);
+     System.out.println("Your BMI:"+ındex);
     }
 }
